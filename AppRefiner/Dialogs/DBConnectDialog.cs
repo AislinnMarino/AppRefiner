245ed5d0f1e8173a69095137db54324c83deeaa2
Ace Marino <aislinnm@buffalo.edu>
2025-05-02 14:44:13 -0400

    Update DBConnectDialog.cs

--- AppRefiner/Dialogs/DBConnectDialog.cs
+++ AppRefiner/Dialogs/DBConnectDialog.cs
@@ -546,7 +546,7 @@ namespace AppRefiner.Dialogs
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Error connecting to database: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show($"Error connecting to database: {ex.Message}", "Error DBName:dbName" +, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 finally
                 {
